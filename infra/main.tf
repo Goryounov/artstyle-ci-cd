ae78fce3d7bca2425aa1238b81b689bfde1ef8a5
Nikita Goryunov <nikita@imobis.ru>
2024-12-17 12:09:58 +0300

    feat: add infra

---
+++ infra/main.tf
@@ -0,0 +1,14 @@
+resource "openstack_compute_instance_v2" "goryunov_infra_tf" {
+  name        = var.instance_name
+  image_name  = var.image
+  flavor_name = var.flavor
+  key_pair    = var.key_name
+
+  network {
+    name = var.network_name
+  }
+
+  security_groups = [var.security_group]
+}
+
+output "instance_ip" { value = openstack_compute_instance_v2.goryunov_infra_tf.access_ip_v4 }
---
+++ infra/variables.tf
@@ -0,0 +1,34 @@
+variable "instance_name" {
+  description = "Name of the instance"
+  default     = "goryunov_infra_tf"
+}
+
+variable "key_name" {
+  description = "Name of the ssh key"
+  default     = "goryunov"
+}
+
+variable "image" {
+  description = "Image to use"
+  default     = "ubuntu-20.04"
+}
+
+variable "flavor" {
+  description = "Flavor of the instance"
+  default     = "m1.small"
+}
+
+variable "network_name" {
+  description = "Network to attach"
+  default     = "sutdents-net"
+}
+
+variable "subnet_name" {
+  description = "Subnet to attach"
+  default     = "students-subnet"
+}
+
+variable "security_group" {
+  description = "Security group to use"
+  default     = "default"
+}
