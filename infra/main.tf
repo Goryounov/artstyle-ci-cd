d7468f906a51c6b194ffa19d93a7e7fa4bc8e66c
Nikita Goryunov <nikita@imobis.ru>
2024-12-18 15:42:36 +0300

    feat: add terraform pprovider

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
 resource "openstack_compute_instance_v2" "goryunov_infra_tf" {
   name        = var.instance_name
   image_name  = var.image
